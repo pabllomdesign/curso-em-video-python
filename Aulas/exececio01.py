3f1a4175766aa157cad8518da75c00fb392e2e2a
Pabllo Martins <pabllomoliveiraa@gmail.com>
2024-06-08 09:08:23 -0300

    📝 Organizando arquivos

--- Aulas/exececio01.py
+++
@@ -1,6 +0,0 @@
-#Criar um script em Python que leia o nome de uma pessoa e escreva uma mensagem de boas vindas de acordo com valor digitado.
-nome=input('Digite seu nome: ')
-print(f'\nOlá {nome}! Seja bem-vindo.')
-
-
-
--- Aulas/exececio03.py
+++
@@ -1,5 +0,0 @@
-#Crie um script em python que leia 02 númeres e retone a soma deles>
-num1=input('Insira o primeiro número: ')
-num2=input('Insira o segundo número: ')
-soma=int(num1) + int(num2)
-print(f'A soma dos valores é {soma}')
