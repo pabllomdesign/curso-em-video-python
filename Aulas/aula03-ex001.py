4393814011997c51a987a72bc39845aed43d3362
Pabllo Martins <pabllomoliveiraa@gmail.com>
2024-06-08 12:38:26 -0300

    ✨ Atividade01: Soma de dois valores

---
+++ Aulas/aula03-ex001.py
@@ -0,0 +1,4 @@
+#Solicite ao usuário dois números e exiba a soma deles.
+n1 = int(input('Informe o primeiro número: '))
+n2 = int(input('Informa o segundo número: '))
+print(f'A some dos valores {n1} e {n2} é: {n1 + n2}')
