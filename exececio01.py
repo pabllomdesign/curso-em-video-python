67f296f6c1dbdb0ed0a93b241b50829b0e9f706a
Pabllo Martins <157547026+pabllomdesign@users.noreply.github.com>
2024-06-07 23:11:30 -0300

    ✨ Exercicios Iniciais

---
+++ exececio01.py
@@ -0,0 +1,6 @@
+#Criar um script em Python que leia o nome de uma pessoa e escreva uma mensagem de boas vindas de acordo com valor digitado.
+nome=input('Digite seu nome: ')
+print(f'\nOlá {nome}! Seja bem-vindo.')
+
+
+
---
+++ exececio02.py
@@ -0,0 +1,6 @@
+#Crie um script em Python que leia o dia, o mês e o ano de nascimento de uma pessoa e escreva a data formatada
+nome='Pabllo'
+dia=input('\nInforme o dia que você nasceu: ')
+mes=input('Agora o mês: ')
+ano=input('por fim, o ano: ')
+print(f'\n{nome}, você nasceu em: ' + dia, mes, ano, sep='/')
