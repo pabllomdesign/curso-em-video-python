31ad3bd84bd3279ffaa6934060b667b2b90901d7
Pabllo Martins <157547026+pabllomdesign@users.noreply.github.com>
2024-06-07 22:13:10 -0300

    ✨ Aula 01 - Função print, declarando váriaveis e input

---
+++ aula01.py
@@ -0,0 +1,18 @@
+#Função print: Mostrando uma msg tipo string no console.
+print('Olá, mundo!')
+
+#Função print: Mostrando uma msg tipo number no console.
+print(7+4)
+
+#Declarando variáveis e mostrando o resultado no console.
+nome = 'Pabllo'
+idade = 30
+peso = 70.5
+
+print('Nome:',nome, 'Idade:',idade,'Peso:',peso)
+
+#Usando o input:
+nome=input('Qual o seu nome?\n')
+idade=input('Qual a sua idade?\n')
+altura=input('Qual a sua altura?\n')
+print(f'Seu nome é {nome}, voce tem {idade} anos e {altura} de altura')
