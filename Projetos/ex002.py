ccc09c4c44e82c1b689884a9f9b884773e5c5cdd
Pabllo Martins <157547026+pabllomdesign@users.noreply.github.com>
2024-06-07 23:31:03 -0300

    ✨ Leia um nome e escreva uma mensagem na tela!

---
+++ Projetos/ex002.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+nome=input('Insira seu nome: ')
+print("\nÉ um prazer te conhecer", nome+"!")
+
+#Usando f-strings (formatação de strings)
+print(f'É um prazer te conhecer {nome}!')
