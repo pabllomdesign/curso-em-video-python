bce8302c2c975c91fc5c7b3c2ad86e521d2ae2a1
Pabllo Martins <157547026+pabllomdesign@users.noreply.github.com>
2024-06-09 14:42:44 -0300

    ✨ Converte temperatura graus Celsius em graus Fahrenheit

---
+++ Projetos/ex014.py
@@ -0,0 +1,5 @@
+#Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+C = float(input('Informa a temperatura em C°: '))
+F = ((9 * C) / 5) + 32
+
+print(f'A temperatura de {C}c° em Fahrenheit é igual {F}fº')
