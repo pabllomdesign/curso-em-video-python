47b3abdc4b588f2def2cff5db58e546cbf947be9
Pabllo Martins <157547026+pabllomdesign@users.noreply.github.com>
2024-06-09 10:12:11 -0300

    ✨ Calcule a media de um aluno

---
+++ Projetos/ex007.py
@@ -0,0 +1,4 @@
+#Desenvolva um algoritmo que leia as duas notas de um aluno e retorne a média
+n1 = float(input('Insira a primeira nota: '))
+n2 = float(input('Insira a segunda nota: '))
+print(f'Sua nota final é: {(n1 + n2)/2}')
