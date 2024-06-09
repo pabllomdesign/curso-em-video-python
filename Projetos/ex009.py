6d320614b4fb48aefe8c2b8449265ef256a068b1
Pabllo Martins <157547026+pabllomdesign@users.noreply.github.com>
2024-06-09 10:34:52 -0300

    ✨ Tabuada

---
+++ Projetos/ex009.py
@@ -0,0 +1,3 @@
+#Faça um programa que leia um numero intero qualquer e mostre sua tabuada na tela
+num = int(input('Insira um número inteiro: '))
+print(f'A tabuada de {num}:\n{num}x0 = {num*0}\n{num}x1 = {num*1}\n{num}x2 = {num*2}\n{num}x3 = {num*3}\n{num}x4 = {num*4}\n{num}x5 = {num*5}\n{num}x6 = {num*6}\n{num}x7 = {num*7}\n{num}x8 = {num*8}\n{num}x9 = {num*9}\n{num}x10 = {num*10}')
