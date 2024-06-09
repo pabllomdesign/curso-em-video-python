a7dd5e9824cb8b4d55d043f06d11c1d3e3932a57
Pabllo Martins <157547026+pabllomdesign@users.noreply.github.com>
2024-06-09 10:18:50 -0300

    ✨ Convertendo metros para cm e mm

---
+++ Projetos/ex008.py
@@ -0,0 +1,3 @@
+#Desenvolva um algoritmo que leia o valor em metros e retone em centrimetros e milimetros
+metragem = float(input('Insira um valor em metros: '))
+print(f'{metragem}m é igual a {metragem*100:.0f}cm e {metragem*1000:.0f}mm')
