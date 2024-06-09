ea237acd5e85e7aef6cfeab808ebf5c1d3ef32fd
Pabllo Martins <157547026+pabllomdesign@users.noreply.github.com>
2024-06-09 10:05:02 -0300

    ✨ Dobro, triplo e raiz quadrada

---
+++ Projetos/ex006.py
@@ -0,0 +1,4 @@
+#Crie um algoritmo qye leia um número e moestre o seu dobro, seu triplo e raiz quadrada.
+
+numero = int(input('Digite um número: '))
+print(f'Você digitou: {numero}\nO dobro de {numero} é {numero*2}\nSeu triplo é: {numero*3}\ne a raiz quadrada é: {numero**0.5:.2f}')
